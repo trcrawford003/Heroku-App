5f270c17d6224afead7f7c269f45412ad857d2ca
Tanner <TRCrawford003@gmail.com>
2023-12-04 16:39:23 -0700

    third commit

--- Main.py
+++ Main.py
@@ -1,19 +1,17 @@
 import flask
-import os
 from flask import send_from_directory
+from flask import abort, redirect, url_for
 
 app = flask.Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/favicon.png')
-
 @app.route('/')
-@app.route('/home')
+@app.route('/docs')
+
+def index():
+    return redirect("https://playversestudio.com/")
+
 def home():
-    
-    return "Hello World!!!"
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
